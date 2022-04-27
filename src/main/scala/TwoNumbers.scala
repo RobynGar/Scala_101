dfad3911a7e8d0cc3feab69d1cb868f59b6e80c9
robyn <robyn.garlington@hotmail.co.uk>
2022-04-27 16:25:07 +0100

    added tests

---
+++ src/main/scala/TwoNumbers.scala
@@ -0,0 +1,21 @@
+class TwoNumbers (x: Int, y: Int){
+
+  def add(): Int = {
+    x + y
+  }
+  def subtract(): Int = {
+    x - y
+  }
+
+  def divide(): Int = {
+    x / y
+//    if (x == 0 || y ==0){
+//      throw new
+//    }
+  }
+
+  def multiply(): Int = {
+    x * y
+  }
+
+}
---
+++ src/test/scala/TwoNumberSpec.scala
@@ -0,0 +1,53 @@
+import org.scalatest.FlatSpec
+
+
+class TwoNumberSpec extends FlatSpec{
+
+  "add" should "add numbers" in {
+    val nums = new TwoNumbers(1, 2)
+    assert(nums.add === 3)	}
+
+  "add" should "add negative numbers" in {
+    val nums = new TwoNumbers(-1, -2)
+    assert(nums.add === -3)	}
+
+  "add" should "add zero numbers" in {
+    val nums = new TwoNumbers(1, 0)
+    assert(nums.add === 1)	}
+
+  "subtract" should "subtract numbers" in {
+    val nums = new TwoNumbers(1, 2)
+    assert(nums.subtract() === -1)	}
+
+  "subtract" should "subtract negative numbers" in {
+    val nums = new TwoNumbers(-1, -2)
+    assert(nums.subtract() === 1)	}
+
+  "subtract" should "subtract zero" in {
+    val nums = new TwoNumbers(1, 0)
+    assert(nums.subtract() === 1)	}
+
+  "divide" should "divide numbers" in {
+    val nums = new TwoNumbers(2, 1)
+    assert(nums.divide() === 2)	}
+
+  "divide" should "divide negative numbers" in {
+    val nums = new TwoNumbers(-2, -1)
+    assert(nums.divide() === 2)	}
+
+  "divide" should "divide by zero" in {
+
+    assertThrows[ArithmeticException](new TwoNumbers(2, 0).divide())	}
+
+  "multiply" should "multiply numbers" in {
+    val nums = new TwoNumbers(2, 2)
+    assert(nums.multiply() === 4)	}
+
+  "multiply" should "multiply negative numbers" in {
+    val nums = new TwoNumbers(-2, -2)
+    assert(nums.multiply() === 4)	}
+
+  "multiply" should "multiply number by zero" in {
+    val nums = new TwoNumbers(2, 0)
+    assert(nums.multiply() === 0)	}
+}
