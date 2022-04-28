d204f13fdceb682eef27cb286e37e1b08380dc0c
robyn <robyn.garlington@hotmail.co.uk>
2022-04-28 12:47:19 +0100

    collections exercise

---
+++ src/main/scala/Collections.scala
@@ -0,0 +1,12 @@
+object Collections extends App{
+  val everybodyNames = Seq("Connie", "Cristian", "Jacob", "Robyn", "Sarina", "Yonis")
+  val randomMap = Map(1 -> "red", 2 -> "yellow", 3 -> "blue", 4 -> "refrigerator")
+  val numSeq = Seq(6,8,3,9,2,7)
+  val addOne = numSeq.map(x => x + 1)
+  val removeAllEvenNums = numSeq.filter(x => x %2 != 0)
+  val characterSeq = Seq("t", "g", "j", "d")
+  val containLetterT = characterSeq.contains("t")
+  println(addOne)
+  println(removeAllEvenNums)
+  println(containLetterT)
+}
