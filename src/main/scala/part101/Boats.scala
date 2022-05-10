26708536e14684f50f20be09852e01db550ba207
robyn <robyn.garlington@hotmail.co.uk>
2022-05-10 16:46:34 +0100

    boat change override

--- src/main/scala/part101/Boats.scala
+++ src/main/scala/part101/Boats.scala
@@ -3,25 +3,18 @@ package part101
 object Boats extends App {
   class Boat(length: Int = 0, width: Int = 0, val topSpeed: Int = 0) {
     val isFasterThan = (a: Boat) => {
-      //      if(Boat.this.topSpeed > a.topSpeed){
-      //        true
-      //      } else if (Boat.this.topSpeed < a.topSpeed){
-      //        false
-      //      } else {
-      //        false
-      //      }
-      Boat.this.topSpeed > a.topSpeed
+     topSpeed > a.topSpeed
     }
   }
 
   trait Artillery {
-    var numGuns: Int
-    var range: Int
+    val numGuns: Int
+    val range: Int
   }
 
   trait RenownedDesigner {
-    var name: String
-    var location: String
+    val name: String
+    val location: String
   }
 
 
@@ -30,15 +23,15 @@ object Boats extends App {
   }
 
   class LuxurySailBoat(hasJacuzzi: Boolean = false, hasBooze: Boolean = false) extends SailBoat with RenownedDesigner {
-    override var name: String = _
-    override var location: String = _
+    override val name: String = ""
+    override val location: String = ""
   }
 
   class PirateShip(numOfGangPlanks: Int = 0) extends SailBoat(5, true, false) with Artillery {
-    override var numGuns: Int = _
-    override var range: Int = _
+    override val numGuns: Int = 0
+    override val range: Int = 0
     val canOutGun = (a: PirateShip) => {
-      PirateShip.this.numGuns > a.numGuns
+      numGuns > a.numGuns
     }
 
     val canFly = (whatTheyAreUsingToFly: String) => {
@@ -52,8 +45,8 @@ object Boats extends App {
 
 
   val theMapleBoat = new LuxurySailBoat(true, false) {
-    name = "Maple the Bear"
-    location = "Brighton"
+    override val name = "Maple the Bear"
+    override val location = "Brighton"
   }
 
   //  println(theMapleBoat.numSails)
@@ -61,8 +54,8 @@ object Boats extends App {
 
   val theGrumpyMoose = new PirateShip(6) {
 
-    numGuns = 8
-    range = 300
+    override val numGuns = 8
+    override val range = 300
     //    hasOars= true
     //    canTrack= false
     //    numSails= 4
@@ -72,10 +65,10 @@ object Boats extends App {
 
   }
   val comparisonPirates = new PirateShip {
-    numGuns = 3
-    range = 500
+    override val numGuns = 3
+    override val range = 500
   }
-  println(theGrumpyMoose.canOutGun(comparisonPirates))
+  println("outgun: " + theGrumpyMoose.canOutGun(comparisonPirates))
   println(theGrumpyMoose.range)
   println("can the ship fly: " + theGrumpyMoose.canFly("wings"))
   println("can the ship fly: " + theGrumpyMoose.canFly("pixie dust"))
@@ -83,3 +76,41 @@ object Boats extends App {
 
 
 }
+//object Boats extends App {
+//
+//  class Boat (val length: Int, val width: Int = 0, val speed: Int = 0) {
+//    def isFaster(newBoatSpeed: Boat): Boolean =
+//      speed > newBoatSpeed.speed
+//  }
+//
+//  trait hasArtillery {
+//    val hasArtillary: Boolean
+//  }
+//
+//  trait hasDesigner {
+//    val hasDesigner: Boolean
+//  }
+//
+// class Sailboat (val numSails: Int = 1, val hasOars: Boolean = true, val canTrack: Boolean = true ) extends Boat(0)
+//
+//  class LuxurySailBoat (val hasJacuzzi: Boolean, val hasBooze: Boolean) extends Sailboat
+//
+//  class PirateShip (val numGangPlanks: Int) extends Sailboat(3,true,true)
+//
+// val luxurySailboatMako = new LuxurySailBoat(true, true) with hasDesigner {
+//   override val hasDesigner: Boolean = true
+//   override val speed: Int = 5
+// }
+//
+//  lazy val pirateShipRolo = new PirateShip(4) with hasArtillery{
+//    override val hasArtillary: Boolean = true
+//    override val speed = 1
+//    override val numSails: Int = 4
+//  }
+//
+//  println(luxurySailboatMako.hasJacuzzi.toString)
+//  println(pirateShipRolo.numGangPlanks.toString)
+//  println(pirateShipRolo.speed.toString)
+//  println(luxurySailboatMako.isFaster(pirateShipRolo).toString)
+//
+//}
--- src/main/scala/part101/Collections.scala
+++ src/main/scala/part101/Collections.scala
@@ -14,62 +14,6 @@ object Collections extends App {
   println(containLetterT)
   println(everybodyNames.flatten)
 
-  //////102 addition//////
-
-  val wildcard = List(1,3,2).map(_ * 2) //_ can be used as a shorthand/ placeholder to represent the input value rather than .map(number => number * 2)
-  println(wildcard)
-  val mapPM = List(1,2,3).map{
-    case 1 => 10
-    case x => x * 2
-  }
-  //map with pattern matching
-  println(mapPM)
-
-  val mapOfMapWithSingleOutput = Map("myKey" -> "myValue").map{case (key, value) => s"key: $key, value: $value"}
-  println(mapOfMapWithSingleOutput)
-
-  val mapOfMapWithTupleOutput = Map("myKey" -> "myValue").map{case (key, value) => value -> key}
-  println(mapOfMapWithTupleOutput)
-
-  // nested collections
- val nestedList = List(2,6,4).map(x => List(x*2, x*4, x*6))
-  println(nestedList)
-  //OR
-  val nestedFlattenList = List(2,6,4).map(x => List(x*2, x*4, x*6)).flatten
-  println(nestedFlattenList)
-  //OR
-  val nestedFlatmapList = List(2,6,4).flatMap(x => List(x*2, x*4, x*6)) //does the same as above but uses one method (flatmap) rather than two (map and flatten)
-  println(nestedFlatmapList)
-
-  val flatmapCase = List(1,2,3).flatMap{
-    case x@(1|3) => Some(x*2)
-      //@ is used for variable binding, binding a variable to a full or partial match result
-    //x is bound to 1 or 3 as the @ is between them
-    //only include the @ when you want to also deal with the object
-    //x at 1 or 3 do this
-    case _       => None
-  }
-  println(flatmapCase)
-
-  //mapping on an option
-  val mapOnOption = Some(3).map(number => number * 3)
-  println(mapOnOption)
-
-  val mapOnOptionCase = Some(3).map{
-    case 1 => 10
-    case x => x * 2
-  }
-  println(mapOnOptionCase)
-
-  val optionEmpty = Option.empty[Int].map(number => number * 3)
-  // if option is empty then return none
-  println(optionEmpty)
-
-  val flatmapIf = Some(3).flatMap(x => if(x == 3) Some(9) else None)
-  println(flatmapIf)
-
-  val flatmapEmptyIf = Option.empty[Int].flatMap(x => if(x == 3) Some(9) else None)
-
 
 
 
