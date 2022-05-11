b4bb44236a16f43323496a4a5d2390b388359636
robyn <robyn.garlington@hotmail.co.uk>
2022-05-11 12:04:55 +0100

    "collection 102 exercises"

--- src/main/scala/part102/Collections.scala
+++ src/main/scala/part102/Collections.scala
@@ -74,13 +74,65 @@ object Collections extends App{
   // if option is empty then return none
   println(optionEmpty)
 
+  //The flatMap function in options is similar to the collections flatMap and is used when the return value is itself an option
   val flatmapIf = Some(3).flatMap(x => if(x == 3) Some(9) else None)
+  val flatmapElse = Some(2).flatMap(x => if(x == 3) Some(9) else None)
   println(flatmapIf)
+  println(flatmapElse)
 
   val flatmapEmptyIf = Option.empty[Int].flatMap(x => if(x == 3) Some(9) else None)
+  println(flatmapEmptyIf)
+
+  //map with either
+
+  val mapEither = Right[String, Int](3).map(number => number * 3)
+  println(mapEither)
+
+  val mapEitherCase = Right[String, Int](1).map{
+    case 1 => 10
+    case x => x * 2
+  }
+  println(mapEitherCase)
+
+  val mapEitherLeft = Left[String, Int]("Hi").map(number => number * 3)
+  println(mapEitherLeft)
+
+  //FLATMAP EITHER
+  println(Right[String, Int](3).flatMap(x => if(x == 3) Right[String, Int](9) else Left[String, Int]("false")))
+  println(Right[String, Int](2).flatMap(x => if(x == 3) Right[String, Int](9) else Left[String, Int]("false")))
+ // println(Right[String, Int]("Hey").flatMap(x => if(x == 3) Right[String, Int](9) else Left[String, Int]("false"))) //this will not compile as we have used Right at the beginning and tried to put a string in it when in the statement Right[String, Int] specifies the Right side will be an Int as the right side of the [] is what the Right statement is expecting
+  println(Left[String, Int]("Hi").flatMap(x => if(x == 3) Right[String, Int](9) else Left[String, Int]("false"))) //this will print Left(Hi) overriding the false written in the left side in the else
 
 
+  //////exercise
+  def capitalMacbooks(macbooks: String): String = {
+    macbooks.map(char => char.toUpper)
+  }
 
+  println(capitalMacbooks("macbooks are the best"))
 
+  def multiplyBy2AndSum(stringList: List[String]): Int = {
+    stringList.map(x => x.toInt).map(n => n * 2).sum
+
+  }
+
+  println(multiplyBy2AndSum(List("2","5","8","9","3")))
+
+  def multiply12OrReturn12(possibleInt: Option[Int]): Int ={
+    possibleInt match {
+      case Some(v) => v * 12
+      case _ => 12
+    }
+  }
+  println(multiply12OrReturn12(Some(3)))
+  println(multiply12OrReturn12(None))
+
+  val scores = Seq(None, Some("A"), Some("B"), Some("C"), None, Some("F"))
+
+  println(scores.filter(x => x != None))
+  println(scores.filter(x => x != None).flatten)// same as above without the some in front of each grade
+
+  println(scores.map(x => if(x == None) Some("F") else x))
+  println(scores.flatMap(x => if(x == None) "F" else x)) //same as above but in the output you do not get all the grades with Some in front of them
 
 }
