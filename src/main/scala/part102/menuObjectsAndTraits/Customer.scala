ed97a32d44f064783fed6977926e6caeb59a0bb6
robyn <robyn.garlington@hotmail.co.uk>
2022-05-17 11:53:59 +0100

    add stars attempt with var

--- src/main/scala/part102/menuObjectsAndTraits/Customer.scala
+++ src/main/scala/part102/menuObjectsAndTraits/Customer.scala
@@ -1,8 +1,8 @@
 package part102.menuObjectsAndTraits
 
-case class Customer(hasLoyaltyCard: Boolean, name: String, numOfStars: Int){
+case class Customer(hasLoyaltyCard: Boolean, name: String, var numOfStars: Int){
   def addStarWhenSpendingOver20(): Unit = {
-    val updatedStars= numOfStars + 1
-    println(s"Welcome back ${name.capitalize}, one star has been added to you loyalty card, you now have $updatedStars")
+    numOfStars += 1
+    println(s"Welcome back ${name.capitalize}, one star has been added to you loyalty card, you now have $numOfStars")
   }
 }
